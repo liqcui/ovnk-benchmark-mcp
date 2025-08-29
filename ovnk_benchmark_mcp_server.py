e335d973c70ea35811784e016fc4587f7857d14b
liqcui <liqcui@redhat.com>
2025-08-29 14:37:33 +0800

    update code

--- ovnk_benchmark_mcp_server.py
+++ ovnk_benchmark_mcp_server.py
@@ -25,11 +25,6 @@ warnings.filterwarnings(
 )
 
 # Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S UTC'
-)
 logger = logging.getLogger(__name__)
 
 from tools.ovnk_benchmark_openshift_general_info import OpenShiftGeneralInfo
