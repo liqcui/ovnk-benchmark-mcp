dc38be8658170b382984921ffe221df591ffdf82
liqcui <liqcui@redhat.com>
2025-09-03 23:43:27 +0800

    update ocd

--- elt/ovnk_benchmark_elt_json2table.py
+++ elt/ovnk_benchmark_elt_json2table.py
@@ -1460,10 +1460,10 @@ class PerformanceDataELT:
         
         # Report top performers from each category
         categories = [
-            ('controller_ready_duration_top5', 'Controller Ready-ovnkube_controller_ready_duration_seconds'),
-            ('node_ready_duration_top5', 'Node Ready-ovnkube_node_ready_duration_seconds'),
-            ('controller_sync_duration_top5', 'Sync Duration-ovnkube_controller_sync_duration_seconds-'),
-            ('controller_sync_service_total_top5', 'Service Rate-ovnkube_controller_sync_service_total')
+            ('controller_ready_duration_top5', 'Controller Ready'),
+            ('node_ready_duration_top5', 'Node Ready'),
+            ('controller_sync_duration_top5', 'Sync Duration'),
+            ('controller_sync_service_total_top5', 'Service Rate')
         ]
         
         for table_name, category_name in categories:
